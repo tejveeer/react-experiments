9cefcf04fecd035c49b239ddea3b0bbafde91d58
Tejveer <tejveerrehsi@gmail.com>
2024-07-20 11:37:32 -0400

    Implemented clear and submit

--- client/src/routes/calculator.js
+++ client/src/routes/calculator.js
@@ -1,3 +1,4 @@
+import { set } from "mongoose";
 import { useState } from "react";
 
 const Calculator = () => {
@@ -19,18 +20,40 @@ const Calculator = () => {
         setNumbers("");
       }
     } else {
+      if (char === "." && numbers.includes(".")) {
+        return;
+      }
       setNumbers(numbers + char);
     }
+  };
+
+  const clear = () => {
+    setExpression("");
+    setNumbers("");
+  };
+
+  const submit = () => {
+    if (numbers[numbers.length - 1] === ".") {
+      return;
+    }
+
+    setNumbers(eval((expression + numbers).replaceAll("x", "*")));
+    setExpression("");
   };
 
   return (
     <div className="flex flex-col justify-center box-border mx-auto w-[20rem] h-screen">
       <div className="overflow-hidden grid gap-1 grid-cols-4 grid-rows-6 mx-0 h-[30rem] border-solid border-[4px] rounded-3xl">
         <div className="text-[2.3rem] pr-1 flex flex-col justify-between text-right relative col-span-4 bg-gray-400">
-          <div className="font-normal italic pt-2 text-[1rem] w-[97%] h-[20%]">{expression}</div>
+          <div className="font-normal italic pt-2 text-[1rem] w-[97%] h-[20%]">
+            {expression}
+          </div>
           {numbers}
         </div>
-        <button className="col-span-3 border-0 rounded-md text-xl">
+        <button
+          className="col-span-3 border-0 rounded-md text-xl"
+          onClick={() => clear()}
+        >
           Clear
         </button>
         {["/", 1, 2, 3, "x", 4, 5, 6, "+", 7, 8, 9, "-", 0, "."].map((char) => (
@@ -41,7 +64,10 @@ const Calculator = () => {
             {char}
           </button>
         ))}
-        <button className="col-span-2 border-0 rounded-md text-xl">
+        <button
+          className="col-span-2 border-0 rounded-md text-xl"
+          onClick={() => submit()}
+        >
           Submit
         </button>
       </div>
