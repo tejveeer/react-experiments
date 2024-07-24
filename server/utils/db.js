d33be6c956e4e3f53f5dde0798bf6a3c4f4395b7
Tejveer Rehsi <tejveerrehsi@gmail.com>
2024-07-24 08:12:06 +0530

    Set up docker compose for postgresql

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,8 +2,8 @@ const postgres = require("postgres");
 
 module.exports = postgres({
   host: "localhost",
-  username: "postgres",
+  username: "tejveer",
   password: "1234",
   database: "test",
-  port: 5433,
+  port: 5432,
 });
