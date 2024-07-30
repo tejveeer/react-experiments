0713773c5506c3897b965a110fa2537178099fd7
Tejveer Rehsi <tejveerrehsi@gmail.com>
2024-07-30 09:34:43 +0530

    Completed server implementation for todo list

--- server/routers/todolist.js
+++ server/routers/todolist.js
@@ -4,6 +4,9 @@ const sql = require('../utils/db');
 const express = require('express');
 const router = express.Router();
 
+const toBoolArr = (arr) =>
+  `{${arr.map((val) => (val ? 'true' : 'false')).join(', ')}}`;
+
 router.get('/', async (req, res) => {
   // should also have middleware for this
   if (!req.cookies?.user) {
@@ -33,10 +36,11 @@ router.post('/add-task', async (req, res) => {
   const { title, description, days, time, repeating } = req.body;
 
   try {
+    console.log(`/add-task ${title}`);
     const [{ id }] = await sql`
       insert into
         tasks (id, user_id, title, description, days, repeating_time, repeating)
-        values (${uuidv4()}, ${userId}, ${title}, ${description}, ${days}, ${time}, ${repeating})
+        values (${uuidv4()}, ${userId}, ${title}, ${description}, ${toBoolArr(days)}, ${time}, ${repeating})
       returning id
     `;
 
@@ -46,13 +50,33 @@ router.post('/add-task', async (req, res) => {
   }
 });
 
+router.post('/delete-task', async (req, res) => {
+  if (!req.cookies?.user) {
+    return res.status(400).end();
+  }
+
+  const { userId } = JSON.parse(req.cookies.user);
+  const { id } = req.body;
+
+  try {
+    await sql`
+      delete from tasks
+        where user_id=${userId} AND id=${id}
+    `;
+
+    res.status(200).end();
+  } catch (e) {
+    res.status(400).end();
+  }
+});
+
 router.put('/change-task', async (req, res) => {
   if (!req.cookies?.user) {
     return res.status(400).end();
   }
 
   const { userId } = JSON.parse(req.cookies.user);
-  const { title, description, days, time, repeating } = req.body;
+  const { id, title, description, days, time, repeating } = req.body;
 
   try {
     await sql`
@@ -60,14 +84,13 @@ router.put('/change-task', async (req, res) => {
         set
           title=${title}, 
           description=${description}, 
-          days=${days}, 
+          days=${toBoolArr(days)}, 
           repeating_time=${time}, 
           repeating=${repeating}
         where
-          user_id = ${userId}
+          user_id = ${userId} AND id=${id}
     `;
 
-    console.log('here');
     res.status(200).end();
   } catch (e) {
     res.status(400).end();
